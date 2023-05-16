d571091d2a56626669cc08e2a24a438c78524663
AsherChen <gpks556@gmail.com>
2023-05-16 17:54:09 +0800

    [GITHUB_PAGES]

--- routers/Card/CardRouter.js
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-import { CardController } from '../../controllers/Card/CardController.js';
-
-export const CardRouter = Router()
-  .post('/', CardController.createOne)
-  .get('/', CardController.readAll)
-  .get('/:id', CardController.readOne)
-  .patch('/:id', CardController.updateOne)
-  .delete('/:id', CardController.deleteOne);
