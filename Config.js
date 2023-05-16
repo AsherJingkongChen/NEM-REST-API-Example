9e39146bd2ddeba80624fcacb4fbea9e1cf02a6e
AsherChen <gpks556@gmail.com>
2023-05-16 12:57:41 +0800

    [SAVE]

--- Config.js
+++
@@ -1,9 +0,0 @@
-import { config } from 'dotenv';
-config();
-
-const { API_VERSION, HOST, PORT } = process.env;
-
-const API_ROUTE = `api/v${API_VERSION}`;
-const DB_URI = process.env[`DB_V${API_VERSION}_URI`];
-
-export { API_ROUTE, DB_URI, HOST, PORT };
